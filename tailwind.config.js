9b131ce8dd81038c528f0801c34e73d9b6288c53
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-01-04 23:58:15 -0800

    added hover update for links and cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,14 @@ module.exports = {
         roseDust: "var(--rose-dust)",
         persianGreen: "var(--persian-green)",
       },
+      boxShadow: {
+        card: "0 1px 3px rgba(0,0,0,.12), 0 1px 2px rgba(0,0,0,.24)",
+        "h-card": "0 10px 28px rgba(0,0,0,.25), 0 8px 10px rgba(0,0,0,.22)",
+        link: "inset 0 -3px 0 var(--primary)",
+        "h-link": "inset 0 -20px 0 var(--primary)",
+        // "link-dark": "inset 0 -4px 0 #b55400",
+        // "link-dark-hover": "inset 0 -18px 0 #b55400",
+      },
     },
   },
   // variants: {
