699fbab83e46cfb34d5a8a39cf7c57a73ef0cc90
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-01-08 23:53:02 -0800

    new component

--- components/index.tsx
+++ components/index.tsx
@@ -4,6 +4,7 @@ export { default as NavBar } from "./library/NavBar";
 export { default as TypedText } from "./library/TypedText";
 export { default as WaveSVG } from "./library/WaveSVG";
 export { default as ExperienceCard } from "./library/ExperienceCard";
+export { default as ProjectsCard } from "./library/ProjectsCard";
 
 export { default as Hero } from "./layouts/Hero";
 export { default as About } from "./layouts/About";
