1d38401d42637b03cdd0df20c017f2bbba073c4c
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-01-08 22:09:36 -0800

    new component export

--- components/index.tsx
+++ components/index.tsx
@@ -3,6 +3,8 @@ export { default as Metatags } from "./library/Metatags";
 export { default as NavBar } from "./library/NavBar";
 export { default as TypedText } from "./library/TypedText";
 export { default as WaveSVG } from "./library/WaveSVG";
+export { default as ExperienceCard } from "./library/ExperienceCard";
+
 export { default as Hero } from "./layouts/Hero";
 export { default as About } from "./layouts/About";
 export { default as Experience } from "./layouts/Experience";
