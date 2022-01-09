a195b0d3431a80b07c8c790e181c2909e36b9289
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-01-09 14:43:27 -0800

    toptracks component to display Top tracks from my account

---
+++ components/library/TopTracks.tsx
@@ -0,0 +1,52 @@
+import useSWR from "swr";
+
+import fetcher from "lib/fetcher";
+
+interface Song {
+  songUrl: string;
+  artist: string;
+  title: string;
+}
+
+interface TopTracks {
+  tracks: Song[];
+}
+
+export default function Tracks() {
+  const { data } = useSWR<TopTracks>("/api/top-tracks", fetcher);
+
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <>
+      {data.tracks.map((track, index) => (
+        <div
+          key={track.songUrl}
+          className="flex flex-row items-baseline border-b border-gray-200 dark:border-gray-800 max-w-3xl w-full mt-8"
+        >
+          <p className="text-sm font-bold text-gray-400 dark:text-gray-600">
+            {index + 1}
+          </p>
+          <div className="flex flex-col pl-3">
+            <a
+              className="font-medium text-gray-900 dark:text-gray-100 truncate w-60 sm:w-96 md:w-full"
+              href={track.songUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {track.title}
+            </a>
+            <p
+              className="text-gray-500 mb-4 truncate w-60 sm:w-96 md:w-full"
+              color="gray.500"
+            >
+              {track.artist}
+            </p>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
