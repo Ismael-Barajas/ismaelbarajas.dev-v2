12495212bc348e5f64732817f52e3626dbc6012c
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-01-09 14:44:05 -0800

    new page for spotify stuff

---
+++ pages/listen.tsx
@@ -0,0 +1,13 @@
+import { NowPlaying, TopTracks } from "components";
+import type { NextPage } from "next";
+
+const Listen: NextPage = () => {
+  return (
+    <div className="container mx-auto min-h-screen-without-nav items-center content-center py-10 ">
+      <NowPlaying />
+      <TopTracks />
+    </div>
+  );
+};
+
+export default Listen;
