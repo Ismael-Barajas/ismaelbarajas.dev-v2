c6865d2de916e1794225c5d3c53ea2df34c79932
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-01-09 00:20:54 -0800

    new footer component

---
+++ components/layouts/Footer.tsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="flex flex-col p-6 justify-center items-center">
+        <p>
+          Built by{" "}
+          <a
+            href="https://github.com/Ismael-Barajas"
+            target="_blank"
+            rel="noreferrer"
+            className="text-persianGreen"
+          >
+            Ismael Barajas
+          </a>
+        </p>
+        {/* <p>Design by</p> */}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
