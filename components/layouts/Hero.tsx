e5ee949068ab502a1e59f8ac79f809e0f875913b
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-01-10 23:06:28 -0800

    using hero ref for now to trigger back to top button

--- components/layouts/Hero.tsx
+++ components/layouts/Hero.tsx
@@ -1,8 +1,14 @@
-import { TypedText, WaveSVG } from "..";
+import { useRef } from "react";
+import { BackToTop, TypedText, WaveSVG } from "..";
 
 const Hero = () => {
+  const heroRef = useRef(null);
   return (
-    <section className="transition-[background-color] duration-700 ease-in-out relative min-h-screen-without-nav items-center content-center flex pb-16 ">
+    <section
+      id="hero"
+      ref={heroRef}
+      className="transition-[background-color] duration-700 ease-in-out relative min-h-screen-without-nav items-center content-center flex pb-16 "
+    >
       <div className="container">
         <h1 className="text-text text-5xl">
           <TypedText
@@ -15,6 +21,7 @@ const Hero = () => {
         </h1>
       </div>
       <WaveSVG />
+      <BackToTop elementRef={heroRef} />
     </section>
   );
 };
