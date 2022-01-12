244fbb97acee3977b57a5eaeab9aa476acd618e2
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-01-11 18:11:07 -0800

    updated container

--- components/layouts/Projects.tsx
+++ components/layouts/Projects.tsx
@@ -3,7 +3,7 @@ import projects from "constants/projects.json";
 
 const Projects = () => {
   return (
-    <div className="container mx-auto min-h-screen-without-nav px-3 pb-16">
+    <div className="container min-h-screen-without-nav pb-12">
       <h2 className="text-center py-5">Projects</h2>
       <div className="grid gap-6 md:gap-8 grid-cols-1 md:grid-cols-2 justify-items-center">
         {projects.map((project, index) => {
