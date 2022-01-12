3cf6ac53a800b661b85fe3b2543a0258f3541cd0
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-01-11 18:10:55 -0800

    updated container

--- components/layouts/About.tsx
+++ components/layouts/About.tsx
@@ -1,11 +1,7 @@
 import { NowPlaying } from "components";
 
 const About = () => {
-  return (
-    <div className="container mx-auto min-h-screen-without-nav items-center content-center py-12 ">
-      About
-    </div>
-  );
+  return <div className="container min-h-screen-without-nav">About</div>;
 };
 
 export default About;
