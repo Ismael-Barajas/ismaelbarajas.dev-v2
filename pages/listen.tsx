13fa0a942a4825e1124621ccda8b23476adcd501
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-01-12 23:20:27 -0800

    added meta tags for listening page

--- pages/listen.tsx
+++ pages/listen.tsx
@@ -1,9 +1,13 @@
-import { NowPlaying, TopTracks } from "components";
+import { Metatags, NowPlaying, TopTracks } from "components";
 import type { NextPage } from "next";
 
 const Listen: NextPage = () => {
   return (
     <div className="container min-h-screen-without-nav items-center content-center py-10">
+      <Metatags
+        title="Jammin out :)"
+        description="What im currently listening too."
+      />
       <NowPlaying />
       <TopTracks />
     </div>
