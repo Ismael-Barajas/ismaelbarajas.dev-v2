feda68960f123c4a672fe7f77a2b61580108e03b
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-01-16 23:14:19 -0800

    testing out age function on /cv page

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -1,5 +1,21 @@
+import { calculateAge } from "lib/age";
+import { useEffect, useState } from "react";
+
 const CV = () => {
-  return <div className="text-text text-center py-12">UNDER CONSTRUCTION</div>;
+  const [age, setAge] = useState("");
+
+  useEffect(() => {
+    const ageInterval = setInterval(() => {
+      setAge(calculateAge());
+    }, 50);
+    return () => clearInterval(ageInterval);
+  }, []);
+
+  return (
+    <div className="text-text text-center py-12">
+      <p>{age}</p>
+    </div>
+  );
 };
 
 export default CV;
