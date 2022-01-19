572019ba27638bce7a46005661638a2b752ddb25
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-01-19 00:22:46 -0800

    added age thing on about section

--- components/layouts/About.tsx
+++ components/layouts/About.tsx
@@ -1,7 +1,23 @@
-import { NowPlaying } from "components";
+import { calculateAge } from "lib/age";
+import { useEffect, useState } from "react";
 
 const About = () => {
-  return <div className="container min-h-screen-without-nav">About</div>;
+  const [age, setAge] = useState("");
+
+  useEffect(() => {
+    const ageInterval = setInterval(() => {
+      setAge(calculateAge());
+    }, 50);
+    return () => clearInterval(ageInterval);
+  }, []);
+
+  return (
+    <div className="container min-h-screen-without-nav">
+      <p className="text-text text-center pt-56 text-4xl">
+        I&apos;m currently <b>{age}</b> years old
+      </p>
+    </div>
+  );
 };
 
 export default About;
