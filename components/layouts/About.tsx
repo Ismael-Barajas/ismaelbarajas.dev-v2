1ad3b8fae8ba4324d9f5fe17068ae96578fc5e9f
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-01-23 15:53:39 -0800

    updated about section

--- components/layouts/About.tsx
+++ components/layouts/About.tsx
@@ -13,9 +13,33 @@ const About = () => {
 
   return (
     <div className="container min-h-screen-without-nav">
-      <p className="text-text text-center pt-56 text-4xl">
-        I&apos;m currently <b>{age}</b> years old
-      </p>
+      <div className="text-text text-center py-12 text-xl">
+        <p>Hi! My name is Ismael Barajas.</p>
+        <br />
+        <p>
+          I&apos;m a {age} year old senior student at California State
+          University, Fullerton. Expected to graduate in May 2022 with an B.S.
+          in Computer Science. When I&apos;m not coding, I enjoy playing video
+          games (unhealthy amount of Valorant) and building/collecting
+          keyboards.
+        </p>
+        <br />
+        <p>
+          As of late I&apos;ve taken an interest in Front End developing with
+          React also into Back End development. You can check out some of the
+          projects I have worked on below! I would say I&apos;m most proficient
+          with React, JavaScript/JSX, Python, and C++. I&apos;m also familiar
+          with other languages as well, such as C. I wouldn&apos;t consider
+          myself a Front End Developer just yet because it&apos;s very early in
+          my career and interests change over time. So who knows what other
+          projects, languages, and career opportunities await!
+        </p>
+        <br />
+        <p>
+          Feel free to contact me anytime through any of the platforms above!
+          Thanks for stopping by!
+        </p>
+      </div>
     </div>
   );
 };
