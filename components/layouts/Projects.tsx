2b4bedbfc89225ceac221b7dde0a171b3d91de60
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-01-23 23:42:41 -0800

    updated projects

--- components/layouts/Projects.tsx
+++ components/layouts/Projects.tsx
@@ -1,11 +1,18 @@
-import { ProjectsCard } from "components";
+import { ProjectsCard, TypedText } from "components";
 import { TechListType } from "components/library/TagIcons";
 import projects from "constants/projects.json";
 
 const Projects = () => {
   return (
     <div className="container min-h-screen-without-nav pb-12">
-      <h2 className="text-center py-5">Projects</h2>
+      <h2 className="font-bold text-center py-5 text-text text-4xl">
+        <TypedText
+          className="animated-underline"
+          strings={["Projects."]}
+          loop={false}
+          whiteSpace={"normal"}
+        />
+      </h2>
       <div className="grid gap-6 md:gap-8 grid-cols-1 md:grid-cols-2 justify-items-center">
         {projects.map((project, index) => {
           return (
