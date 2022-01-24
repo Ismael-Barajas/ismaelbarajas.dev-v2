d871217ddc601fe27f968f5615f5196ada436cdc
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-01-23 23:42:26 -0800

    updated experience

--- components/layouts/Experience.tsx
+++ components/layouts/Experience.tsx
@@ -1,12 +1,18 @@
 import experience from "constants/experience.json";
-import { ExperienceCard } from "components";
+import { ExperienceCard, TypedText } from "components";
 import { TechListType } from "components/library/TagIcons";
 
 const Experience = () => {
   return (
     <div className="container min-h-screen-without-nav pb-12">
-      {/* TODO: Header text needs to stand out more think about it */}
-      <h2 className="text-center py-5">Experience</h2>
+      <h2 className="font-bold text-center py-5 text-text text-4xl">
+        <TypedText
+          className="animated-underline"
+          strings={["Experience."]}
+          loop={false}
+          whiteSpace={"normal"}
+        />
+      </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8">
         {experience.map((job, index) => {
           return (
@@ -16,6 +22,7 @@ const Experience = () => {
               position={job.position}
               tags={job.tags as Array<TechListType>}
               key={index}
+              url={job.url}
             />
           );
         })}
