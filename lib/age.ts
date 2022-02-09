4ce12d5c1762968481b3d7cae1ab1a3f5cacf25f
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-02-09 10:32:30 -0800

    updated age for now

--- lib/age.ts
+++ lib/age.ts
@@ -1,5 +1,5 @@
 export const calculateAge = () => {
-  var t = new Date().valueOf() - new Date(1997, 1, 30).valueOf();
+  var t = new Date().valueOf() - new Date(1997, 1).valueOf();
   t /= 1000 * 60 * 60 * 24 * 365;
   return `${Math.floor(t)}.${(t % 1).toFixed(9).substring(2)}`;
 };
