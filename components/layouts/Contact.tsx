62d76765d61c702a3fca29e9ab729ffa2fcd4a44
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-06-01 11:43:59 -0700

    now opens link on new tab, simplified code

--- components/layouts/Contact.tsx
+++ components/layouts/Contact.tsx
@@ -2,6 +2,29 @@ import { TypedText } from "components";
 import { FaTwitter, FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 
+const contactInfo = [
+  {
+    icon: <FaLinkedin className="h-10 w-10" />,
+    url: "https://www.linkedin.com/in/ismael-barajas/",
+  },
+  {
+    icon: <FaGithub className="h-10 w-10" />,
+    url: "https://github.com/Ismael-Barajas",
+  },
+  {
+    icon: <IoIosMail className="h-10 w-10" />,
+    url: "mailto:ismaelbarajas.dev@gmail.com",
+  },
+  {
+    icon: <FaTwitter className="h-10 w-10" />,
+    url: "https://twitter.com/inxanee",
+  },
+  {
+    icon: <FaInstagram className="h-10 w-10" />,
+    url: "https://instagram.com/lnxanee",
+  },
+];
+
 const Contact = () => {
   return (
     <div className="container min-h-screen-without-nav">
@@ -15,36 +38,18 @@ const Contact = () => {
       </h2>
       <div className="flex justify-center items-center content-center min-h-screen-contact">
         <div className="flex gap-4 lg:gap-6 justify-center">
-          <a
-            href="https://www.linkedin.com/in/ismael-barajas/"
-            className="flex items-center justify-center rounded-full h-16 w-16 hover:bg-secondary text-text content-center transition-all hover:ring-2 ring-offset-indigo-100 dark:ring-gray-200"
-          >
-            <FaLinkedin className="h-10 w-10" />
-          </a>
-          <a
-            href="https://github.com/Ismael-Barajas"
-            className="flex items-center justify-center rounded-full h-16 w-16 hover:bg-secondary text-text content-center transition-all hover:ring-2 ring-offset-indigo-100 dark:ring-gray-200"
-          >
-            <FaGithub className="h-10 w-10" />
-          </a>
-          <a
-            href="mailto:ismaelbarajas.dev@gmail.com"
-            className="flex items-center justify-center rounded-full h-16 w-16 hover:bg-secondary text-text content-center transition-all hover:ring-2 ring-offset-indigo-100 dark:ring-gray-200"
-          >
-            <IoIosMail className="h-10 w-10" />
-          </a>
-          <a
-            href="https://twitter.com/InXanee"
-            className="flex items-center justify-center rounded-full h-16 w-16 hover:bg-secondary text-text content-center transition-all hover:ring-2 ring-offset-indigo-100 dark:ring-gray-200"
-          >
-            <FaTwitter className="h-10 w-10" />
-          </a>
-          <a
-            href="https://www.instagram.com/lnxanee/"
-            className="flex items-center justify-center rounded-full h-16 w-16 hover:bg-secondary text-text content-center transition-all hover:ring-2 ring-offset-indigo-100 dark:ring-gray-200"
-          >
-            <FaInstagram className="h-10 w-10" />
-          </a>
+          {contactInfo.map((contact, index) => {
+            return (
+              <a
+                href={contact.url}
+                className="flex items-center justify-center rounded-full h-16 w-16 hover:bg-secondary text-text content-center transition-all hover:ring-2 ring-offset-indigo-100 dark:ring-gray-200"
+                target={`_blank`}
+                key={index}
+              >
+                {contact.icon}
+              </a>
+            );
+          })}
         </div>
       </div>
     </div>
