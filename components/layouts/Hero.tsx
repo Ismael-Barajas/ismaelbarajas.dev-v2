4c8a77f73181b2d5794bcd24e43639bebac6f4be
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-07-29 00:09:53 -0500

    Hero Edit

--- components/layouts/Hero.tsx
+++ components/layouts/Hero.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { useRef } from "react";
 import { BackToTop, TypedText, WaveSVG } from "..";
 
@@ -7,19 +8,39 @@ const Hero = () => {
     <section
       id="hero"
       ref={heroRef}
-      className="transition-[background-color] duration-700 ease-in-out relative min-h-screen-without-nav items-center content-center flex pb-16 "
+      className="transition-[background-color] duration-700 ease-in-out relative min-h-screen-without-nav items-center content-center flex pb-44 "
     >
       <div className="container">
-        <h1 className="text-text text-5xl text-center">
+        <h2 className="text-text text-2xl md:text-4xl 2xl:text-5xl">Hi!</h2>
+        {/* <h1 className="text-text text-5xl text-center">
           <TypedText
             strings={[
               "Hi!^750",
-              "Site is still being worked on so some stuff might be broken/missing oops :3",
+              "My name is Ismael but you can call me ish or ishy!",
             ]}
             loop={false}
             whiteSpace={"normal"}
           />
+        </h1> */}
+        <h1 className="text-text mt-1 text-3xl md:text-5xl 2xl:text-6xl">
+          My name is{" "}
+          <span className="animated-underline text-primary ">Ismael</span> but
+          you can call me{" "}
+          <TypedText
+            strings={["ish^2000 or ishy! :3"]}
+            loop={false}
+            whiteSpace={"normal"}
+          />
         </h1>
+        <p
+          className={clsx(
+            "mt-4 max-w-4xl text-gray-700 dark:text-gray-200 md:mt-6",
+            "md:text-lg 2xl:text-xl"
+          )}
+        >
+          {/* I work with React Ecosystem, and write to teach people how to rebuild
+          and redefine fundamental concepts through mental models. */}
+        </p>
       </div>
       <WaveSVG />
       <BackToTop elementRef={heroRef} />
